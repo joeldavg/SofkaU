c1500240d029a9288c15ba11bc02870cd2f85068
Joel Guzman <joel-guzman@hotmail.com>
2022-04-14 13:56:17 -0500

    Sentencias If/Else

---
+++ src/main/java/com/sofka/flujoDeControl/IfElse.java
@@ -0,0 +1,25 @@
+package com.sofka.flujoDeControl;
+
+public class IfElse {
+    /*
+        >, >=, <, <=, ==, !=, ||, &&
+     */
+    public static void main(String[] args) {
+        int var = 6;
+        int var2 = 10;
+        boolean cond;
+
+        if ( !((var == 6) || (var2 > 5)) ) {
+            cond = true;
+        } else {
+            cond = false;
+        }
+        if (cond) {
+            System.out.println("Tu operacion es verdadera");
+        } else {
+            System.out.println("Tu operacion es falsa");
+        }
+
+    }
+
+}
