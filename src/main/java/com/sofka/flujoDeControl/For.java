8624af063baaf6dd1329ec5b24bba420c8ed4bd5
Joel Guzman <joel-guzman@hotmail.com>
2022-04-14 14:07:36 -0500

    Instrucción FOR

---
+++ src/main/java/com/sofka/flujoDeControl/For.java
@@ -0,0 +1,14 @@
+package com.sofka.flujoDeControl;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            i *= 2;
+            System.out.println("El valor de i es: " + i);
+        }
+
+    }
+
+}
