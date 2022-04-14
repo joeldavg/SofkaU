b1e1a2ac017ce578b24ac4fd12b3c4dc4d08dc8e
Joel Guzman <joel-guzman@hotmail.com>
2022-04-14 12:13:52 -0500

    Variables y Operaciones Aritméticas

---
+++ src/main/java/com/sofka/variables/TiposDeDatos.java
@@ -0,0 +1,22 @@
+package com.sofka.variables;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+
+        short suma;
+        suma = 3+10;
+        int resta = 8-17;
+        long residuo = 9%2;
+        float multiplicacion = 2*(15*(-2));
+        double division = 10/3.4;
+
+        System.out.println("El resultado de la suma es: " + suma);
+        System.out.println("El resultado de la resta es: " + resta);
+        System.out.println("El resultado de la multiplicacion es: " + multiplicacion);
+        System.out.println("El resultado de la division es: " + division);
+        System.out.println("El residuo de la division es: " + residuo);
+
+    }
+
+}
