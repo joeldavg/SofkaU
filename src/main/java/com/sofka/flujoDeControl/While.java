81dd25246ca090726d89f2fd0a34148982169207
Joel Guzman <joel-guzman@hotmail.com>
2022-04-14 14:15:22 -0500

    Instrucción While

---
+++ src/main/java/com/sofka/flujoDeControl/While.java
@@ -0,0 +1,20 @@
+package com.sofka.flujoDeControl;
+
+public class While {
+
+    public static void main(String[] args) {
+
+        boolean cond = true;
+        int x = 0;
+        while (cond) {
+            System.out.println("El valor de x es: " + x);
+            x += 2;
+
+            if (x > 8) {
+                cond = false;
+            }
+        }
+
+    }
+
+}
