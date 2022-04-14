a2abc6be0e9fa69348c740949ac9592c731d91fd
Joel Guzman <joel-guzman@hotmail.com>
2022-04-14 14:24:41 -0500

    Instruccines Break y Continue

---
+++ src/main/java/com/sofka/flujoDeControl/Break.java
@@ -0,0 +1,27 @@
+package com.sofka.flujoDeControl;
+
+public class Break {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ejemplo con break");
+        for (int i = 0; i <= 5; i++) {
+
+            if (i == 3) {
+                break;
+            }
+            System.out.println("El valor de i es: " + i);
+        }
+
+        System.out.println("Ejemplo con continue");
+        for (int i = 0; i <= 5; i++) {
+
+            if (i == 3) {
+                continue;
+            }
+            System.out.println("El valor de i es: " + i);
+        }
+        System.out.println("Has dejado el ciclo for");
+    }
+
+}
